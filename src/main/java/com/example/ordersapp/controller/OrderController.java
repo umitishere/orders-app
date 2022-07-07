c87621e5eec93ce47ebd7426fddf7e08bd125bde
Ümit Sancaklı <umitsancakli98@gmail.com>
2022-07-07 16:03:37 +0300

    Create controller to get orders

---
+++ src/main/java/com/example/ordersapp/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.example.ordersapp.controller;
+
+import com.example.ordersapp.data.Order;
+import com.example.ordersapp.service.OrderService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/orders")
+public class OrderController {
+
+  private final OrderService orderService;
+
+  @Autowired
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+  @GetMapping
+  public List<Order> getOrders() {
+    return orderService.getOrders();
+  }
+
+}
