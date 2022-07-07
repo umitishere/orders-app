bc57ebe483fb6d2618f2a753a6e9130d34f509ed
Ümit Sancaklı <umitsancakli98@gmail.com>
2022-07-07 15:02:21 +0300

    Initialize the project with specified dependencies

---
+++ src/main/java/com/example/ordersapp/OrdersAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ordersapp/OrdersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
