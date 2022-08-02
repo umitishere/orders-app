7d9d0ce38b7e140f00840aae9bb7fbf45e3c24cf
umitishere <umitsancakli98@gmail.com>
2022-08-02 10:24:35 +0300

    Reduce boilerplate code with using Lombok

--- src/main/java/com/example/ordersapp/data/Order.java
+++ src/main/java/com/example/ordersapp/data/Order.java
@@ -1,13 +1,18 @@
 package com.example.ordersapp.data;
 
-import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -19,76 +24,5 @@ public class Order {
   private String orderName;
   private Float orderPrice;
   private Integer orderAmount;
-
-  public Order() {
-
-  }
-
-  public Order(Long id, String orderCategory, String orderName, Float orderPrice,
-      Integer orderAmount) {
-    this.id = id;
-    this.orderCategory = orderCategory;
-    this.orderName = orderName;
-    this.orderPrice = orderPrice;
-    this.orderAmount = orderAmount;
-  }
-
-  public Order(String orderCategory, String orderName, Float orderPrice, Integer orderAmount) {
-    this.orderCategory = orderCategory;
-    this.orderName = orderName;
-    this.orderPrice = orderPrice;
-    this.orderAmount = orderAmount;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getOrderCategory() {
-    return orderCategory;
-  }
-
-  public void setOrderCategory(String orderCategory) {
-    this.orderCategory = orderCategory;
-  }
-
-  public String getOrderName() {
-    return orderName;
-  }
-
-  public void setOrderName(String orderName) {
-    this.orderName = orderName;
-  }
-
-  public Float getOrderPrice() {
-    return orderPrice;
-  }
-
-  public void setOrderPrice(Float orderPrice) {
-    this.orderPrice = orderPrice;
-  }
-
-  public Integer getOrderAmount() {
-    return orderAmount;
-  }
-
-  public void setOrderAmount(Integer orderAmount) {
-    this.orderAmount = orderAmount;
-  }
-
-  @Override
-  public String toString() {
-    return "Order{" +
-        "id=" + id +
-        ", orderCategory='" + orderCategory + '\'' +
-        ", orderName='" + orderName + '\'' +
-        ", orderPrice=" + orderPrice +
-        ", orderAmount=" + orderAmount +
-        '}';
-  }
 
 }
